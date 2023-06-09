b5bae36f8e0641bc46b3ecba744364858fa234a6
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2023-06-09 01:59:45 -0700

    last check the bot is now officially stable
    hopefully

--- commands/misc/interactionbased/credit.js
+++ commands/misc/interactionbased/credit.js
@@ -9,6 +9,7 @@ module.exports = {
   {field:Noblox.js:> The backend of the group based commands are built proud with noblox.js <3\n> **Server invite:** .gg/qKPTduAP\n [Website](https://noblox.js.org/):yes }
   {field:Bcrypt:> Used to encrypt the Auth/Console key and Cookie so that if there is a breach which is unlikely your data is safe <3\n> **Server invite:** None?\n [Website](https://www.npmjs.com/package/bcrypt):yes }
   {field:RoSerices:> This bot is based off RoServices their bot made me make my own <3\n> **Server invite:** .gg/rSmDHhpfkQ\n [Website](https://roservices.app/):yes }
+  {field:Dodogames:> My friend also stole some commands <3\n> **Server invite:** .gg/vYdpYu9JaQ \n [Website](https://www.youtube.com/watch?v=dQw4w9WgXcQ):yes }
   {field:Lightner:>  My friend of 2 years crazy long tbh also helped me with the project <3\n> **Server invite:** None? lightner dm me if u want a server here\n [Website](https://www.youtube.com/watch?v=dQw4w9WgXcQ):yes }
   {color:#C3A78E}}{extraOptions:{interaction}};no]
   `
